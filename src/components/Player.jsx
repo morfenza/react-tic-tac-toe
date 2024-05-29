d801c39a68b4bb6edb5e02e42597ce8cee8956f1
Ian Lourenço <ianlourenco@protonmail.com>
2024-05-29 14:37:33 -0300

    feat: create Player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handlePlayerNameChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  function handleEditClick() {
+    setIsEditing((wasEditing) => !wasEditing);
+  }
+
+  let playerNameField = <span className="player-name">{playerName}</span>;
+  let buttonCaption = "Edit";
+
+  if (isEditing) {
+    playerNameField = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handlePlayerNameChange}
+      />
+    );
+
+    buttonCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerNameField}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
+    </li>
+  );
+}
