1a858d663d922ac67d4e611ffe1661fb44a7ac56
Ian Lourenço <ianlourenco@protonmail.com>
2024-06-05 11:53:12 -0300

    chore: change active player state into derived data

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,24 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 
+function derivedActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = derivedActivePlayer(gameTurns);
 
   function handleGameBoardCells(rowIndex, cellIndex) {
-    setActivePlayer((currentActivePlayer) =>
-      currentActivePlayer === "X" ? "O" : "X"
-    );
-
     setGameTurns((previousTurns) => {
-      let currentPlayer = "X";
-
-      if (previousTurns.length > 0 && previousTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      let currentPlayer = derivedActivePlayer(previousTurns);
 
       const updatedTurns = [
         { square: { row: rowIndex, cell: cellIndex }, player: currentPlayer },
